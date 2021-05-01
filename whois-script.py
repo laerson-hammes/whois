8de1cdbfba3e254e0851d5412879beeae5d9455c
laerson-hammes <laersonha@gmail.com>
2021-05-01 18:16:38 -0300

    OOP

--- whois-script.py
+++ whois-script.py
@@ -1,27 +1,31 @@
-# https://pypi.org/project/python-whois/
-import whois
+import whois # type: ignore
 import argparse
 
 
-def scan_whois(domain: str) -> None:
-    try:
-        consult = whois.whois(domain)
-        print(consult.text)
-        with open(domain + ".txt", "w") as f:
-            f.write(consult.text)
-    except Exception as e:
-        print(e)
+class Whois(object):
+    def __init__(self, /) -> None:
+        self.scan_whois(self.get_arguments())
+        
+
+    def scan_whois(self, domain: str, /) -> None:
+        try:
+            consult = whois.whois(domain)
+            print(consult.text)
+            with open(domain + ".txt", "w") as f:
+                f.write(consult.text)
+        except Exception as e:
+            raise Exception(e)
 
 
-def get_arguments() -> str:
-   parser = argparse.ArgumentParser()
-   parser.add_argument("-d", "--domain", dest="domain", help="Domain name")
-   options = parser.parse_args()
-   if not options.domain:
-      domain: str = str(input("Domain name: "))
-      return domain
-   return options.domain
+    def get_arguments(self, /) -> str:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-d", "--domain", dest="domain", help="Domain name")
+        options = parser.parse_args()
+        if not options.domain:
+            domain: str = str(input("Domain name: "))
+            return domain
+        return options.domain
 
 
 if __name__ == "__main__":
-    scan_whois(domain := get_arguments())
+    w = Whois()
